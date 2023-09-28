d0564e36cfb6f913a8217951b5e9620c8759b667
Bishwajit <130075034+bishwajit006@users.noreply.github.com>
2023-09-28 16:12:07 +0600

    Add files via upload

---
+++ University CGPA Calculator (Average CGPA).c
@@ -0,0 +1,34 @@
+/* Write a programme to calculate the average CGPA of a student*/
+#include<stdio.h>
+int main()
+{
+    int semester,subject,n,i,j;
+    double grade,credit,totalgrade,semestercredit,semestercg,totalcg=0,totalcredit=0,r;
+    printf("Enter the number of Semester : ");
+    scanf("%d",&semester);
+    for(i=1;i<=semester;i++)
+    {
+       semestercredit=totalgrade=semestercg=grade=credit=r=0.0;
+        printf("\n\n\nSEMESTER-%d",i);
+        printf("\n\nEnter the Number of Subjects : ");
+        scanf("%d",&subject);
+        for(j=1;j<=subject;j++)
+        {
+            printf("\nEnter the credit of the subject-%d : ",j);
+            scanf("%lf",&credit);
+            semestercredit+=credit;
+            printf("\nEnter the CGPA of the subject-%d : ",j);
+            scanf("%lf",&grade);
+            r=(credit*grade);
+            totalgrade+=r;
+        }
+        semestercg=(totalgrade/semestercredit);
+        printf("\nSEMESTER-%d : CGPA = %.2lf",i,semestercg);
+        totalcg+=(semestercg*semestercredit);
+        totalcredit+=semestercredit;
+    }
+    printf("\n\n\n-----------------------------------");
+    printf("\n  The Average CGPA/SGPA : %.2lf",totalcg/totalcredit);
+    printf("\n-----------------------------------\n\n\n\n\n");
+    return 0;
+}
